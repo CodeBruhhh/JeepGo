ffe9f2a21a786a685420949c7f4517cbc73ceb8a
CodeBruhhh <christianpescadero1@gmail.com>
2025-10-26 14:38:51 +0800

    fixed home bug

--- app/(tabs)/fares.tsx
+++ app/(tabs)/fares.tsx
@@ -3,8 +3,8 @@ import { Text, View } from 'react-native'
 
 const fares = () => {
   return (
-    <View className="flex-1  items-center bg-secondary">
-          <Text className="text-5xl text-primary top-64 font-bold">Fares</Text>
+    <View className="flex-1 items-center bg-secondary">
+        <Text className="text-5xl text-primary top-64 font-bold">Fares</Text>
     </View>
   )
 }
--- app/(tabs)/history.tsx
+++ app/(tabs)/history.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native'
 
 const history = () => {
   return (
-    <View className="flex-1  items-center bg-secondary">
+    <View className="flex-1 items-center bg-secondary">
       <Text className="text-5xl text-primary top-64 font-bold">History</Text>
     </View>
   )

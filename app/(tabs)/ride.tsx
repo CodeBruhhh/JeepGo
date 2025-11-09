09f81805b6bb100acb63a8e4596587dc367fcfea
CodeBruhhh <christianpescadero1@gmail.com>
2025-11-09 22:19:33 +0800

    map-test

--- app/(tabs)/ride.tsx
+++ app/(tabs)/ride.tsx
@@ -1,10 +1,11 @@
+import MapScreen from '@/components/Map'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { View } from 'react-native'
 
 const ride = () => {
   return (
     <View className="flex-1  items-center bg-secondary">
-          <Text className="text-5xl text-primary top-64 font-bold">Ride</Text>
+          <MapScreen />
     </View>
   )
 }

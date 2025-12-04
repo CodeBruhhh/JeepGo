331f60cda3deb40bf75966522d19bb871810fdf1
CodeBruhhh <christianpescadero1@gmail.com>
2025-12-04 17:07:26 +0800

    bug fixes

--- app/(tabs)/routes.tsx
+++ app/(tabs)/routes.tsx
@@ -290,13 +290,6 @@ const routes = () => {
     bottom: 0,
   };
 
-  // Ensure header/footer are restored when leaving this screen
-  useEffect(() => {
-    return () => {
-      setShowRideButton(true);
-    };
-  }, [setShowRideButton]);
-
   return (
     <View className="flex-1">
       {/* Map View */}
